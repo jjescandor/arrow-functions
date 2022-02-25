23fd379f6743b3ac4d20a09e9e340184a5e554c2
JJ Escandor <jj.p.escandor@gmail.com>
2022-02-25 00:38:29 -0500

    step 11

--- app.js
+++ app.js
@@ -156,7 +156,7 @@ console.log(joe);
 
 
 
-/*Student.prototype.greeting = function () {
+Student.prototype.greeting = function () {
   return `Hi, my name is ${this.name}`;
 };
 
@@ -168,12 +168,10 @@ console.log(joe.greeting());
 
 
 
-Student.courseName = function () {
-  return 'This student is enrolled in Code 301.';
-};
+Student.courseName = () => 'This student is enrolled in Code 301.';
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
+console.log(Student.courseName());
 
 
 
@@ -184,18 +182,18 @@ Student.prototype.scope = function () {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+//undefined
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+//undefined
 // 3. Explain why "this" is different when an arrow function is used.
-//
-*/
+//because Arrow fucntions do not have prototype property
+
